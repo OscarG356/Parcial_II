79c9dc14e9fd9fac045a056d97fca58fff41b3d0
Oscar Gutiérrez <oscar.gutierrezr@udea.edu.co>
2020-12-14 08:07:14 -0500

    Reutilizo codigo con unas ligeras modificaciones para el punto 4

--- Parcial/main.cpp
+++ Parcial/main.cpp
@@ -12,6 +12,7 @@ void print(int,int,float,float,int);
 void DisparoOfensivo(int, int, int, int);
 void DisparoDefensivo(int, int, int, int);
 void DisparoDefensivo2(int ,int , int , int , int);
+void DisparoDefensivo3(int ,int,int, int, int, int);
 
 int main()
 {    
@@ -48,6 +49,11 @@ int main()
             break;
         case 3:{
             DisparoDefensivo2(ofensivo->getHi(),defensivo->getD(),defensivo->getHi(),31,85);
+            system("pause");
+            system("cls");
+        }
+        case 4:{
+            DisparoDefensivo3(0,ofensivo->getHi(),defensivo->getD(),defensivo->getHi(),31,85);
             system("pause");
             system("cls");
         }
@@ -207,6 +213,77 @@ void DisparoDefensivo2(int Yo,int Xd, int Yd, int Angl, int Vo0)
                     print(VI0,t,x,y,angle);
                     flag += 1;
                     VI0 += 50;
+                    break;
+                }
+                if(y < 0){
+                    break;
+                }
+            }
+            if(flag == 3) break;
+
+        }
+        if(flag == 3) break;
+    }
+    if(flag != 3){
+        cout << "No impacto en los disparos esperados"<< endl;
+    }
+}
+
+void DisparoDefensivo3(int Xo,int Yo,int Xd, int Yd, int Angl, int Vo0){
+
+    Bala *disparo_d = new Bala(Xd,Yd,true); //disparo
+    Bala *disparo_o = new Bala(Xo,Yo,false);
+
+    int flag = 0;
+    bool flag2 = 0;
+    float x,y,x2,y2;
+    float aux,auy;
+    float Vx1,Vy1, Vx2,Vy2;
+    int V0o = 0;
+    int t = 0;
+    int angle = 0;
+    Vx2 = Vo0*cos((Angl)*pi/180);
+    Vy2 = Vo0*sin((Angl)*pi/180);
+    for(V0o = Vo0; ; V0o += 5){
+        for(angle = 0; angle < 90; angle++){
+            Vx1 = V0o*cos((angle+90)*pi/180);
+            Vy1 = V0o*sin((angle+90)*pi/180);
+            x = 0.0;
+            y = 0.0;
+            x2 = 0.0;
+            y2 = 0.0;
+            for(t = 0; ; t++){
+                x2 = Vx2*(t+2);
+                y2 = Yo + Vy2*(t+2) -(0.5*G*(t+2)*(t+2));
+                x = Xd+Vx1*t;
+                y = Yd + Vy1*t -(0.5*G*t*t);
+                for(int t2 = t; ;t2++){
+                    aux = Xd+Vx1*t2;
+                    auy = Yd + Vy1*t2 -(0.5*G*t2*t2);
+                    float Rang=disparo_d->getRang();
+                    if(disparo_d->impacto(x,y,aux,auy,Rang)){
+                        flag2 = 1;
+                        break;
+                    }
+                    if(auy < 0){
+                        break;
+                    }
+                }
+                if(flag2){
+                    flag2 = 0;
+                    break;
+                }
+                float Rang=disparo_o->getRang();
+                if(disparo_d->impacto(Xd,Yd,x2,y2,Rang)){
+                    break;
+                }
+                if(disparo_d->impacto(x2,y2,x,y,Rang)){
+                    if(y<0) y = 0;
+                    print(V0o,t+2,x2,y2,Angl);
+                    cout << "_________________________________"<<endl;
+                     print(V0o,t,x,y,angle);
+                    flag += 1;
+                    V0o += 50;
                     break;
                 }
                 if(y < 0){
